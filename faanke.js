5c0eb47232354d35a5ce2f518c4dfc08413bc800
linkel <ironcarbide@gmail.com>
2019-01-06 11:43:52 -0800

    start, stop, and reset functional

--- faanke.js
+++ faanke.js
@@ -1,4 +1,5 @@
 const tomato_display = document.querySelector(".tomato-display");
+const timer_display = document.querySelector(".display");
 
 for (let i = 0; i <= 48; i++) {
     const tomato = document.createElement('img');
@@ -10,34 +11,62 @@ for (let i = 0; i <= 48; i++) {
 // Timer
 
 let isRunning = false;
+let timer_fn;
+const TIMER_TIME = 25;
+let time_remaining = 0;
 
 const timer_start = () => {
     if (isRunning) {
-        console.log("shouldn't start again...")
         return
     } else {
-        console.log("start running")
         isRunning = true;
     }
-    // Grabbing current time and calculating 25 min into the future
-    const date = new Date();
-    const TIMER_TIME = 25; // 25 minutes baby
-    const date_future = new Date(date.getTime() + TIMER_TIME*60000);
+    let date_future;
+    // Grabbing current time and calculating 25 min into the future if there's no stored remaining time
+    if (time_remaining === 0) {
+        const date = new Date();
+        date_future = new Date(date.getTime() + TIMER_TIME*60000);
+    } else {
+        const date= new Date();
+        date_future = new Date(date.getTime() + time_remaining*60000);
+    }
 
-    const timer_fn = setInterval(() => {
-        // Get today's date and time
+    timer_fn = setInterval(() => {
         const now = new Date().getTime();
         // Find the distance between now and the future date
         const distance = date_future - now;
+        // Time remaining is used for the stop functionality to restart at where we left off
+        time_remaining = (distance % (1000 * 60 * 60)) / (1000 * 60);
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.querySelector(".display").textContent = ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+        timer_display.textContent = ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
         if (distance < 0) {
             clearInterval(timer_fn);
-            document.querySelector(".display").textContent = "00:00";
+            timer_display.textContent = "00:00";
             isRunning = false;
           }
     }, 100);
+}
+
+const timer_stop = () => {
+    if (isRunning) {
+        clearInterval(timer_fn);
+        isRunning = false;
+    } else {
+        return // won't do anything if the timer isn't running
+    }
+}
+
+const timer_reset = () => {
+    if (isRunning) {
+        clearInterval(timer_fn);
+        time_remaining = 0;
+        isRunning = false;
+        timer_display.textContent = "25:00";
+    } else {
+        time_remaining = 0;
+        timer_display.textContent = "25:00";
+    }
 }
 
 start_button = document.querySelector("#start");
@@ -45,3 +74,5 @@ stop_button = document.querySelector("#stop");
 reset_button = document.querySelector("#reset");
 
 start_button.addEventListener("click", () => timer_start());
+stop_button.addEventListener("click", () => timer_stop());
+reset_button.addEventListener("click", () => timer_reset());
