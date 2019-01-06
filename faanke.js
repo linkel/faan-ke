6935e906a809253fe65cc240a158d9a22a326205
linkel <ironcarbide@gmail.com>
2019-01-06 11:25:04 -0800

    start button begins timer.

--- faanke.js
+++ faanke.js
@@ -9,19 +9,39 @@ for (let i = 0; i <= 48; i++) {
 
 // Timer
 
-// Grabbing current time and calculating 25 min into the future
-const date = new Date();
-const timestamp = date.getTime();
-const TIMER_TIME = 25 // 25 minutes baby
-const date_future = new Date(date.getTime() + TIMER_TIME*60000);
+let isRunning = false;
 
-const timer_fn = setInterval(() => {
-      // Get todays date and time
-    const now = new Date().getTime();
-    // Find the distance between now and the count down date
-    const distance = date_future - now;
-    console.log(distance);
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.querySelector(".display").textContent = ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
-}, 100);
+const timer_start = () => {
+    if (isRunning) {
+        console.log("shouldn't start again...")
+        return
+    } else {
+        console.log("start running")
+        isRunning = true;
+    }
+    // Grabbing current time and calculating 25 min into the future
+    const date = new Date();
+    const TIMER_TIME = 25; // 25 minutes baby
+    const date_future = new Date(date.getTime() + TIMER_TIME*60000);
+
+    const timer_fn = setInterval(() => {
+        // Get today's date and time
+        const now = new Date().getTime();
+        // Find the distance between now and the future date
+        const distance = date_future - now;
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.querySelector(".display").textContent = ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+        if (distance < 0) {
+            clearInterval(timer_fn);
+            document.querySelector(".display").textContent = "00:00";
+            isRunning = false;
+          }
+    }, 100);
+}
+
+start_button = document.querySelector("#start");
+stop_button = document.querySelector("#stop");
+reset_button = document.querySelector("#reset");
+
+start_button.addEventListener("click", () => timer_start());
